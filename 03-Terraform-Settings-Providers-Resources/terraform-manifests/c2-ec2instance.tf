7dc0643679b3286c2a2e02dc696e52ec6000f180
amityadav1986 <amit.databridge@gmail.com>
2023-12-22 16:09:04 +0530

    terraform resource blcok added

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,9 +1,6 @@
 # Resource: EC2 Instance
-resource "aws_instance" "myec2vm" {
-  ami = "ami-0742b4e673072066f"
-  instance_type = "t3.micro"
+resource "aws_instance" "myec2_amit" {
+  ami = "ami-0a0f1259dd1c90938"
+  instance_type = "t2.micro"
   user_data = file("${path.module}/app1-install.sh")
-  tags = {
-    "Name" = "EC2 Demo"
-  }
 }
