c7fd9ac657625b1edd6071c4c696d3df4c151f32
amityadav1986 <amit.databridge@gmail.com>
2023-12-22 16:23:41 +0530

    Addecd Name tag to aws_instance resourc

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -3,4 +3,6 @@ resource "aws_instance" "myec2_amit" {
   ami = "ami-0a0f1259dd1c90938"
   instance_type = "t2.micro"
   user_data = file("${path.module}/app1-install.sh")
+  tags = {
+    Name = "my_ec2"
 }
